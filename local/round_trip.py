1c4bae1a190a06a46662f1a8b15d266a57be752c
Simon Billinge <simon.billinge@gmail.com>
2020-02-06 18:50:57 -0500

    initial commit of round_trip in public

---
+++ local/round_trip.py
@@ -0,0 +1,52 @@
+import sys
+
+import yaml
+import argparse
+from pathlib import Path
+
+from regolith.fsclient import dump_yaml
+
+THIS_DIR = Path('.')
+collsdir = THIS_DIR / ".." / ".." / "rg-db-public" / "db"
+
+
+def parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--collections", nargs='+',
+                        help="collections or collections to round-trip "
+                             "e.g., -c people contacts expenses")
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parser()
+    if not args.collections:
+        sys.exit("please specify a collection, or list of collections, using "
+                 "the -c switch, e.g., -c people contacts expenses")
+    if isinstance(args.collections, str):
+        collections = [args.collections]
+    else:
+        collections = args.collections
+    needed_dbs = collections
+
+    for coll in needed_dbs:
+        fullcoll = coll + ".yml"
+        collfile = collsdir / fullcoll
+        with open(collfile, "r", encoding='utf8') as i:
+            current = yaml.safe_load(i)
+        sync_coll(collfile, current)
+        print("{} has been roundtripped".format(coll))
+
+
+def sync_coll(collfile, dict):
+    with open(collfile, "r", encoding='utf8') as i:
+        current = yaml.safe_load(i)
+    current.update(dict)
+    for k, v in current.items():
+        v.update({"_id": k})
+    dump_yaml(collfile, current)
+
+
+if __name__ == '__main__':
+    main()
